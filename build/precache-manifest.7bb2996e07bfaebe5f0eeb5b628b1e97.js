642e70984b07a56795f285d88dc27292308ced23
Pirates <ican.notfound@gmail.com>
2020-08-05 13:02:18 +0800

    ready deploy v.0.2

---
+++ build/precache-manifest.7bb2996e07bfaebe5f0eeb5b628b1e97.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3fd8daf6141ea8b1eefbbe8abe897df",
+    "url": "/Frontend-literasi/index.html"
+  },
+  {
+    "revision": "b96b6120580f6b068177",
+    "url": "/Frontend-literasi/static/css/main.b6230ef9.chunk.css"
+  },
+  {
+    "revision": "1bc1ce6c8786fd332080",
+    "url": "/Frontend-literasi/static/js/2.07019953.chunk.js"
+  },
+  {
+    "revision": "be6629bd7ab7e127b5d76ac590c4478e",
+    "url": "/Frontend-literasi/static/js/2.07019953.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b96b6120580f6b068177",
+    "url": "/Frontend-literasi/static/js/main.6e13843f.chunk.js"
+  },
+  {
+    "revision": "211f1fc06e8382d6b19a",
+    "url": "/Frontend-literasi/static/js/runtime-main.6ca0ff9e.js"
+  }
+]);
